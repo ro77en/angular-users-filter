f378329b2aa8f504664c6e52564a6ed68d54c161
ro77en <lucas.mdantas@icloud.com>
2024-06-11 12:52:01 -0300

    user details template binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'users-filter';
+  userSelected: IUser = UsersList[0];
 }
