79c25b008aa83bd1de9ebca97197dc5f67c974dd
ro77en <lucas.mdantas@icloud.com>
2024-06-11 17:12:26 -0300

    simulating http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
+import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
+
+  usersList: IUser[] = [];
+
+  // Simulando HTTP Request
+  ngOnInit() {
+    setTimeout(() => {
+      this.usersList = UsersList;
+    }, 3000);
+  }
 
   onUserSelected(user: IUser) {
     this.userSelected = user;
