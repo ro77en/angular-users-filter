ed93f836e67d28b77cf9a295b222c3cbe27efca6
ro77en <lucas.mdantas@icloud.com>
2024-06-11 16:08:59 -0300

    phone and address pipes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,10 +6,11 @@ import { AngularMaterialModule } from '../angular-material/angular-material.modu
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { FilterComponent } from './filter/filter.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
   declarations: [UserDetailsComponent, FilterComponent, UsersListComponent],
-  imports: [AngularMaterialModule, FormsModule, CommonModule],
+  imports: [AngularMaterialModule, FormsModule, CommonModule, PipesModule],
   exports: [UserDetailsComponent, FilterComponent, UsersListComponent],
 })
 export class ComponentsModule {}
--- src/app/data/users-list.ts
+++ src/app/data/users-list.ts
@@ -13,7 +13,7 @@ export const UsersList: IUser[] = [
       estado: 'SP',
       pais: 'Brasil',
     },
-    telefone: '11912345678',
+    telefone: '1112345678',
     ativo: true,
     funcao: 'Desenvolvedor',
     dataCadastro: '2023-08-01T09:00:00.000Z',
