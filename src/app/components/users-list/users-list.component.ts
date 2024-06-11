4d06d134dd74c235d3946460615a9aab4373554a
ro77en <lucas.mdantas@icloud.com>
2024-06-11 17:00:58 -0300

    hiding user details initially

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { IUser } from '../../interfaces/user/user.interface';
 import { UsersList } from '../../data/users-list';
 
@@ -11,7 +11,10 @@ export class UsersListComponent {
   displayedColumns: string[] = ['name', 'date', 'status'];
   usersList: IUser[] = UsersList;
 
+  @Output('userSelected') userSelectedEmit = new EventEmitter<IUser>();
+
   onUserSelected(user: IUser) {
     console.log('user', user);
+    this.userSelectedEmit.emit(user);
   }
 }
