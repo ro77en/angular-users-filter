74f1bb560f6d1b4cd1e465d91428b541f1524511
ro77en <lucas.mdantas@icloud.com>
2024-06-11 12:13:29 -0300

    filter component layout

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,9 +1,34 @@
 import { NgModule } from '@angular/core';
+
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [MatListModule, MatDividerModule],
-  exports: [MatListModule, MatDividerModule],
+  imports: [
+    MatListModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
+  exports: [
+    MatListModule,
+    MatDividerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
 })
 export class AngularMaterialModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
-  declarations: [UserDetailsComponent],
-  imports: [AngularMaterialModule],
-  exports: [UserDetailsComponent],
+  declarations: [UserDetailsComponent, FilterComponent],
+  imports: [AngularMaterialModule, FormsModule, CommonModule],
+  exports: [UserDetailsComponent, FilterComponent],
 })
 export class ComponentsModule {}
